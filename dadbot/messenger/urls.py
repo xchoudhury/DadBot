4d05705eb321755978f276c6f651fd9473f35275
Xavier Choudhury <xchoudhury@gmail.com>
2018-12-30 13:44:55 -0500

    got verification and endpoint working

--- dadbot/messenger/urls.py
+++ dadbot/messenger/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^get/', views.get_message),
+    url(r'^', views.message),
     ]
--- dadbot/messenger/views.py
+++ dadbot/messenger/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import status
 
-@api_view(['GET'])
-def get_message(request):
-    return Response(request.data)
+def validate_request(request):
+    return True
+    dadbot_token = "not secure"
+    # dadbot_token = os.environ['dadbot_token']
+
+    if 'hub.mode' not in request.query_params \
+    or 'hub.verify_token' not in request.query_params \
+    or 'hub.challenge' not in request.query_params:
+        return False
+
+    mode = request.query_params['hub.mode']
+    token = request.query_params['hub.verify_token']
+
+    if mode == 'subscribe' and token == dadbot_token:
+        return True
+        # return Response(request.query_params['hub.challenge'], status=status.HTTP_200_OK)
+    
+    return False
+
+@api_view(['GET', 'POST'])
+def message(request):
+    if not validate_request(request):
+        error = {
+            "error" : "some error info"
+        }
+        return Response(error, status=status.HTTP_403_FORBIDDEN)
+    
+    # Might have to do this in a new thread
+    body = request.data
+    if body['object'] == 'page':
+        for entry in body['entry']:
+            event = entry['messaging'][0]
+            print(event)
+
+    return Response(status=status.HTTP_200_OK)
